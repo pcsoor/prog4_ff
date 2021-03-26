456777798325aa134f470c4db61a5fd6f1f0041f
csoorpeterfrank <c80ld7@stud.uni-obuda.hu>
2021-03-26 19:39:16 +0100

    Game model interface added.

---
+++ TetrisMario/GameModel/IGameModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameModel
+{
+    interface IGameModel
+    {
+        int[,] Board { get; set; }
+        string Block { get; set; }
+    }
+}
