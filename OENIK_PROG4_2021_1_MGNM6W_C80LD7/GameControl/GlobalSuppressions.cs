9d534eb846113d4103a3b4d0864bfa523d2a80f2
csoorpeterfrank <c80ld7@stud.uni-obuda.hu>
2021-04-16 16:53:27 +0200

    delete prev

--- OENIK_PROG4_2021_1_MGNM6W_C80LD7/GameControl/GlobalSuppressions.cs
+++
@@ -1,7 +0,0 @@
-﻿// <copyright file="GlobalSuppressions.cs" company="MGNM6W_C80LD7">
-// Copyright (c) MGNM6W_C80LD7. All rights reserved.
-// </copyright>
-
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("", "CA1014", Justification = "<NikGitStats>", Scope = "module")]
