a8ddfff45638d989dc1d166954e8bcc8d76fc413
FarkasErik <farkaserik24@stud.uni-obuda.hu>
2021-04-16 16:49:27 +0200

    Apha

---
+++ OENIK_PROG4_2021_1_MGNM6W_C80LD7/GameModel/Enums.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameModel
+{
+    public class Enums
+    {
+        public enum UI
+        {
+            Wall = 1,
+            Player = 7,
+            BasicBlock = 2,
+            Undecided = 0
+        }
+
+        public enum Types
+        {
+            Player,
+            Block,
+            Wall,
+            Undecided,
+            Empty
+        }
+
+        public enum Directions
+        {
+            Left,
+            Right,
+            Up,
+            Dowm,
+            LowerLeft,
+            LowerRight,
+            UpperLeft,
+            UpperRight,
+            Null
+        }
+
+        public enum eWaitTime
+        {
+            Player = 0,
+            PlayerJump = -150,
+            PlayerRecover = -50,
+            Box = -100
+        }
+    }
+}
