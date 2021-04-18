c64da0d7d1d0e71190882a95ced1445167e6b25d
csoorpeterfrank <c80ld7@stud.uni-obuda.hu>
2021-04-18 23:09:22 +0200

    add: Logic test class

---
+++ OENIK_PROG4_2021_1_MGNM6W_C80LD7/TetrisMario.Logic.Tests/GameTests.cs
@@ -0,0 +1,31 @@
+﻿// <copyright file="GameTests.cs" company="MGNM6W_C80LD7">
+// Copyright (c) MGNM6W_C80LD7. All rights reserved.
+// </copyright>
+
+namespace TetrisMario.Logic.Tests
+{
+    using Moq;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Logic tests.
+    /// </summary>
+    [TestFixture]
+    public class GameTests
+    {
+        /// <summary>
+        /// SetUp method.
+        /// </summary>
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        /// <summary>
+        /// Test spawn method.
+        /// </summary>
+        public void TestSpawnBlock()
+        {
+        }
+    }
+}
